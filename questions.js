a8cb52d1efc75fd2a48f93f732082806e2e05c3a
ramon-braga <ramonvbraga@gmail.com>
2023-10-29 19:57:13 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,95 @@
+let questions = [
+    {
+        question: 'In which HTML element do we place the javascript?',
+        options: [
+            'js tag',
+            'script tag',
+            'scripting tag',
+            'javascript tag'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Where is the right place to put the javascript?',
+        options: [
+            'In the body tag',
+            'In the head tag',
+            'Both the body and head tags are correct'
+        ],
+        answer: 2
+    },
+    {
+        question: 'What is the correct syntax to add an external javascript file to the script tag?',
+        options: [
+            'name="xxx.js"',
+            'src="xxx.js"',
+            'href="xxx.js"'
+        ],
+        answer: 1
+    },
+    {
+        question: 'An external javascript file must have the script tag',
+        options: [
+            'True',
+            'False'
+        ],
+        answer: 1
+    },
+    {
+        question: 'How to write "Hello World" in an alertbox?',
+        options: [
+            'alertBox("Hello World")',
+            'msgBox("Hello World")',
+            'msg("Hello World")',
+            'alert("Hello World")'
+        ],
+        answer: 3
+    },
+    {
+        question: 'How do you create a function in Javascript?',
+        options: [
+            'function:myFunction()',
+            'function = myFunction()',
+            'function myFunction()'
+        ],
+        answer: 2
+    },
+    {
+        question: 'How to call a function called "minhaFuncao"?',
+        options: [
+            'call myFunction',
+            'call function myFunction',
+            'myFunction()'
+        ],
+        answer: 2
+    },
+    {
+        question: 'How to write an IF conditional in Javascript?',
+        options: [
+            'if i = 5',
+            'if i == 5 then',
+            'if (i == 5)',
+            'if i = 5 then'
+        ],
+        answer: 2
+    },
+    {
+        question: 'How to make an if that executes a code if "i" is different from 5',
+        options: [
+            'if (i != 5)',
+            'if (i <> 5)',
+            'if i <> 5',
+            'if i =! 5 then'
+        ],
+        answer: 0
+    },
+    {
+        question: 'How does the while loop start?',
+        options: [
+            'while (i <= 10)',
+            'while (i <= 10; i++)',
+            'while i = 1 to 10'
+        ],
+        answer: 0
+    },
+];
